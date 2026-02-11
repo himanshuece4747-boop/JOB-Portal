3fbc22c2f73280720034b675b15e4aeddb1acd94
Avinash <avinash2596360@gmail.com>
2026-02-11 14:14:23 +0530

    Analytics added

--- frontend/job-portal/src/LandingPage/LandingPage.jsx
+++ frontend/job-portal/src/LandingPage/LandingPage.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Analytics from './components/Analytics'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Features from './components/Features'
+// import Header from './components/Header'
+// import Hero from './components/Hero'
+// import Features from './components/Features'
 import { Briefcase } from 'lucide-react'
 
 const LandingPage = () => {
   return (
     <div className='min-h-screen mb-[100vh]'>
-      <Header />
+      {/* <Header />
       <Hero />
-      <Features/>
+      <Features/> */}
       <Analytics />
     </div>
   )
